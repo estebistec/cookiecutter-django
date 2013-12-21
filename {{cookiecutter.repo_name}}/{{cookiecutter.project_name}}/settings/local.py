892af33c1ffd444e76d764599312145608170249
Steven Cummings <estebistec@gmail.com>
2013-12-21 16:36:37 -0600

    Remove stray extra space in generated project local settings

--- {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/settings/local.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/settings/local.py
@@ -47,7 +47,7 @@ LOGGING = {
 # http://django-debug-toolbar.readthedocs.org/
 
 INSTALLED_APPS += ('debug_toolbar',)
-MIDDLEWARE_CLASSES += ( 'debug_toolbar.middleware.DebugToolbarMiddleware',)
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 INTERNAL_IPS = ('127.0.0.1',)
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False
